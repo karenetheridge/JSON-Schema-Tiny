c7ebc105dc9600efc78ccbc1e53d7735886da84d
Karen Etheridge <ether@cpan.org>
2021-12-30 18:13:58 -0800

    also mention the slack servers

--- lib/JSON/Schema/Tiny.pm
+++ lib/JSON/Schema/Tiny.pm
@@ -1420,4 +1420,11 @@ SOURCES.>
 * L<https://json-schema.org>
 * L<Understanding JSON Schema|https://json-schema.org/understanding-json-schema>: tutorial-focused documentation
 
+=head1 SUPPORT
+
+=for stopwords OpenAPI
+
+You can also find me on the L<JSON Schema Slack server|https://json-schema.slack.com> and L<OpenAPI Slack
+server|https://open-api.slack.com>, which are also great resources for finding help.
+
 =cut
