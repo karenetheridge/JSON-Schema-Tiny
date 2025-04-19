4cd228744e18d9b95064bf374222aee443a681e0
Karen Etheridge <ether@cpan.org>
2025-04-19 13:33:53 -0700

    increment $VERSION after 0.029 release

--- lib/JSON/Schema/Tiny.pm
+++ lib/JSON/Schema/Tiny.pm
@@ -5,7 +5,7 @@ package JSON::Schema::Tiny;
 # ABSTRACT: Validate data against a schema, minimally
 # KEYWORDS: JSON Schema data validation structure specification tiny
 
-our $VERSION = '0.029';
+our $VERSION = '0.030';
 
 use 5.020;  # for unicode_strings, signatures, postderef features
 use stable 0.031 'postderef';
